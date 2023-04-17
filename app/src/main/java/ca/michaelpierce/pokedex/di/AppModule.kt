72838702ad711904ac1165aa77c971653a52b930
mpierce1988 <mpierce1988@gmail.com>
2023-04-17 20:36:04 -0300

    Added provides methods for Repo and API in AppModule

--- app/src/main/java/ca/michaelpierce/pokedex/di/AppModule.kt
+++ app/src/main/java/ca/michaelpierce/pokedex/di/AppModule.kt
@@ -1,8 +1,15 @@
 package ca.michaelpierce.pokedex.di
 
+import ca.michaelpierce.pokedex.data.remote.PokeApi
+import ca.michaelpierce.pokedex.repository.PokemonRepository
+import ca.michaelpierce.pokedex.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 // Annotated as Dagger Module, and InstallIn SingletonComponent to make each
 // dependency in this module a Singleton
@@ -10,4 +17,22 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // Provide classes for repo and api
+    // use provide at start of function name
+    // Annotate with Singleton and Provides
+    @Provides
+    @Singleton
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Provides
+    @Singleton
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
 }
---
+++ app/src/main/java/ca/michaelpierce/pokedex/util/Constants.kt
@@ -0,0 +1,5 @@
+package ca.michaelpierce.pokedex.util
+
+object Constants {
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+}
