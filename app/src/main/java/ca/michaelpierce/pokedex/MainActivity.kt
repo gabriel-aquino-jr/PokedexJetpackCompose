8c9a9745f3eca3202fe0d215923ecaa671e19393
mpierce1988 <mpierce1988@gmail.com>
2023-04-17 19:47:02 -0300

    Created NavHost

--- app/src/main/java/ca/michaelpierce/pokedex/MainActivity.kt
+++ app/src/main/java/ca/michaelpierce/pokedex/MainActivity.kt
@@ -7,15 +7,52 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import ca.michaelpierce.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme() {
+                val navController = rememberNavController()
 
+                NavHost(navController = navController,
+                    startDestination = "pokemon_list_screen") {
+                    composable("pokemon_list_screen") {
+
+                    }
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                    arguments = listOf(
+                        navArgument("dominantColor") {
+                            type = NavType.IntType
+                        },
+                        navArgument("pokemonName") {
+                            type = NavType.StringType
+                        }
+                    )) {
+                        // Retrieve color and name from navhost composable
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            // Convert color int into a Composable Color
+                            // If cannot convert, assign white
+                            color?.let { Color(it) } ?: Color.White
+                        }
+
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
+                }
             }
         }
     }
